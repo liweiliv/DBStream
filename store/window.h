a9ed2236321d85b9648cfc2dfe6160cb456501b4
liweiliv <liwei960067495@sina.com>
2019-04-20 20:04:12 +0800

    Create window.h

---
+++ store/window.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "stream.h"
+namespace STORE {
+	class window :public DBStream {
+	public:
+		enum windowType {
+			W_TUMBLING,
+			W_SLIDING,
+			W_SESSION
+		};
+	protected:
+		DBStream * m_sourceStream;
+		windowType m_type;
+	};
+	class countWindow :public window {
+	private:
+		uint32_t m_count;
+	};
+	class timeWindow :public window {
+		uint32_t m_milSeconds;
+	};
+}
