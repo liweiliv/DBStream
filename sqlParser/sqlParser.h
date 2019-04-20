9f22f2c7de1c83acaaedca8fe58a61d57011229c
liweiliv <liwei960067495@sina.com>
2019-04-20 18:42:28 +0800

    Create sqlParser.h

---
+++ sqlParser/sqlParser.h
@@ -0,0 +1,46 @@
+/*
+ * sqlParser.h
+ *
+ *  Created on: 2018年11月21日
+ *      Author: liwei
+ */
+
+#ifndef SQLPARSER_H_
+#define SQLPARSER_H_
+#include <string>
+#include <map>
+#include "sqlParserHandle.h"
+//todo
+#define OS_WIN 1
+#ifdef  OS_WIN
+#include <wtypes.h>
+#endif //  OS_WIN
+
+class jsonValue;
+namespace SQL_PARSER
+{
+class SQLWord;
+class sqlParser
+{
+private:
+    std::map<uint32_t, SQLWord *> m_parseTree;
+    std::map<uint32_t, SQLWord *> m_parseTreeHead;
+#ifdef OS_LINUX
+    void * m_funcsHandle;
+#else ifdef OS_WIN
+	HINSTANCE m_funcsHandle;
+#endif
+    SQLWord* loadSQlWordFromJson(jsonValue *json);
+	void (*m_initUserDataFunc)(handle *h);
+	void (*m_destroyUserDataFunc)(handle *h);
+public:
+    sqlParser();
+    ~sqlParser();
+    int LoadFuncs(const char * fileName);
+    int LoadParseTree(const char *config);
+    int LoadParseTreeFromFile(const char * file);
+public:
+    parseValue parse(handle *&h, const char * sql);
+};
+};
+#endif /* SQLPARSER_H_ */
