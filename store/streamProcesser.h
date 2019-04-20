36296997e88ba06f55b4d782a7f68bed8a1b5aef
liweiliv <liwei960067495@sina.com>
2019-04-20 20:02:25 +0800

    Create streamProcesser.h

---
+++ store/streamProcesser.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <stdint.h>
+#include <string>
+#include "job.h"
+namespace STORE {
+	class iterator;
+	class streamProcesser:public jobProcess
+	{
+	private:
+		std::string m_define;
+		uint16_t m_sourceItersCount;
+		iterator ** m_sourceIters;
+	public:
+		bool process()
+		{
+
+		}
+	};
+}
