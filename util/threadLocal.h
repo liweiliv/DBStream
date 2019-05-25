5840b43ecfbe6134d0381a1b57390e7dda70582f
liweiliv <liwei960067495@sina.com>
2019-05-25 18:34:47 +0800

    Create threadLocal.h

---
+++ util/threadLocal.h
@@ -0,0 +1,44 @@
+#pragma once
+#include"likely.h"
+static constexpr int maxThreadCount = 256;
+static thread_local int threadid = maxThreadCount+1;
+bool initLocalThreadId();
+template<class T>
+class threadLocal
+{
+protected:
+	T* m_var[maxThreadCount];
+	virtual T * createVar()
+	{
+		return new T;
+	}
+public:
+	threadLocal()
+	{
+		memset(m_var, 0, sizeof(m_var));
+	}
+	virtual ~threadLocal() 
+	{
+		for (int i = 0; i < maxThreadCount; i++)
+		{
+			if (m_var[i] != nullptr)
+				delete m_var[i];
+		}
+	}
+	inline T* get()
+	{
+		if (unlikely(threadid >= maxThreadCount))
+		{
+			if (!initLocalThreadId())
+				return nulptr;
+		}
+		if (likely(m_var[threadid]!=nullptr))
+		{
+			return m_var[threadid];
+		}
+		else
+		{
+			return (m_var[threadid] = createVar());
+		}
+	}
+};
