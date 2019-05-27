73ba2fc61065345b51d746b9e97f8b612a273cc8
liweiliv <liwei960067495@sina.com>
2019-05-27 09:44:14 +0800

    Update filter.h

--- store/filter.h
+++ store/filter.h
@@ -10,23 +10,50 @@
 #include "../meta/metaDataCollection.h"
 #include "../message/record.h"
 namespace STORE{
+	#define 	FL_CHECKPOINT		0x01
+	#define 	FL_TABLE_ID			0x02
+	#define		FL_CONSTRANT		0x04
+	#define		FL_COMMON_COLUMN	0x08
+	#define		FL_RECORD_TYPE		0x10
+	#define		FL_MULTI_TABLE		0x20
     struct filter{
-        unsigned char * filters;
-        unsigned int size;
-        filter(){
+		uint32_t m_filterType;
+		uint64_t m_startCheckpoint;
+		uint64_t m_endCheckpoint;
+		uint64_t * m_tableIdWhiteList;
+		uint32_t m_tableIdWhiteListSize;
+		uint64_t * m_tableIdBlackList;
+		uint32_t m_tableIdBlackListSize;
+		uint8_t m_typeBitmap[DATABASE_INCREASE::RecordType::R_MAX_RECORD_TYPE >> 3 + (DATABASE_INCREASE::RecordType::R_MAX_RECORD_TYPE & 0x7) ? 1 : 0];
+        filter(uint32_t filterType):m_filterType(filterType){
 
         }
         int init(const char *filters)
         {
 
         }
-        virtual bool doFilter(const char * data){
-
-        }
-        bool continer(const char *filterString)
-        {
-
-        }
+		inline bool onlyNeedGeneralInfo()
+		{
+			return m_filterType & (!(FL_CHECKPOINT | FL_TABLE_ID | FL_RECORD_TYPE));
+		}
+		inline bool filterByTableID(uint64_t tableID)//todo
+		{
+			return true;
+		}
+		inline bool filterByGeneralInfo(uint64_t tableId,uint8_t recordType,uint64_t LogOffset,uint64_t timestamp)
+		{
+			if (!TEST_BITMAP(m_typeBitmap, recordType))
+				return false;
+			if ((m_filterType&FL_CHECKPOINT) && (m_startCheckpoint > LogOffset || m_endCheckpoint < LogOffset))
+				return false;
+			if (m_filterType&FL_TABLE_ID && !filterByTableID(tableId))
+				return false;
+			return true;
+		}
+		inline bool filterByRecord(DATABASE_INCREASE::record * record)
+		{
+			return true;
+		}
     };
 }
 
