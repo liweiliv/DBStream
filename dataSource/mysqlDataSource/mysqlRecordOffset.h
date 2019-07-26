336044d7de5c3b6add0452cc10d84dc697694067
liweiliv <liwei960067495@sina.com>
2019-07-26 18:17:20 +0800

    Update mysqlRecordOffset.h

--- dataSource/mysqlDataSource/mysqlRecordOffset.h
+++ dataSource/mysqlDataSource/mysqlRecordOffset.h
@@ -1,18 +1,22 @@
 #pragma once
+#include <stdint.h>
 namespace DATA_SOURCE
 {
+	static constexpr uint8_t LP_OFFSET = 44;
+	static constexpr uint64_t MAX_LOG_ID = 1ULL << LP_OFFSET;
 	static inline uint64_t createMysqlRecordOffset(uint32_t logId, uint64_t logOffset)//max support 
 	{
-		assert(logOffset < (1UL << 44));
-		return (static_cast<uint64_t>(logId) << 44) | (logOffset);
+
+		assert(logOffset < MAX_LOG_ID);
+		return (static_cast<uint64_t>(logId) << LP_OFFSET) | (logOffset);
 	}
 	static inline uint64_t offsetInFile(uint64_t recordOffset)
 	{
-		return recordOffset & ((1UL << 44) - 1);
+		return recordOffset & (MAX_LOG_ID - 1);
 	}
 	static inline uint32_t fileId(uint64_t recordOffset)
 	{
-		return recordOffset >> 44;
+		return recordOffset >> LP_OFFSET;
 	}
 }
 
