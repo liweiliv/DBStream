95bca289223d3d884eca7d365cd1557842d8bfed
liweiliv <liwei960067495@sina.com>
2019-08-16 17:43:48 +0800

    Update sqlWord.cpp

--- sqlParser/sqlWord.cpp
+++ sqlParser/sqlWord.cpp
@@ -4,33 +4,18 @@
 #include "util/winString.h"
 #include "glog/logging.h"
 namespace SQL_PARSER {
-	void SQLSingleWord::success(handle* h, const std::string& matchedWord)
-	{
-		if (h != nullptr)
-		{
-			if (m_sqlType != UNSUPPORT)
-				h->type = m_sqlType;
-			if (m_parser != nullptr)
-			{
-				statusInfo* s = new statusInfo;
-				s->sql = matchedWord;
-				s->parserFunc = m_parser;
-				if (h->head == nullptr)
-					h->head = s;
-				else
-					h->end->next = s;
-				h->end = s;
-			}
-		}
-	}
 	SQLSingleWord* SQLSingleWord::create(bool optional, const std::string& str)
 	{
 		if (str == "_A_")
 			return new SQLArrayWord(optional);
 		else if (str == "_AS_")
 			return new SQLAnyStringWord(optional);
-		else if (str == "_N_")
-			return new SQLNameWord(optional);
+		else if (str == "_DB_")
+			return new SQLDBNameWord(optional);
+		else if (str == "_TABLE_")
+			return new SQLTableNameWord(optional);
+		else if (str == "_COLUMN_")
+			return new SQLColumnNameWord(optional);
 		else if (str == "_B_")
 			return new SQLBracketsWord(optional);
 		else if (str == "_NUM_")
@@ -42,11 +27,11 @@ namespace SQL_PARSER {
 		else
 			return nullptr;
 	}
-	parseValue SQLCharWord::match(handle* h, const char*& sql)
+	SQLValue* SQLCharWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
 		parseValue rtv = OK;
-		std::string matchedWord;
+		SQLStringValue* value = MATCH;
 		char c = *p;
 		if (c >= 'A' && c <= 'Z')
 			c += 'a' - 'A';
@@ -55,43 +40,172 @@ namespace SQL_PARSER {
 			LOG(ERROR) << m_comment << " do not match " << sql;
 			return NOT_MATCH;
 		}
-		if (m_parser != nullptr)
-			matchedWord = std::string(p, 1);
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLStringValue(SQLValue::STRING_TYPE);
+			value->value.assign(p, 1);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
 		LOG(ERROR) << m_comment << "match " << sql;
 		sql = p + 1;
-		success(h, matchedWord);
-		return OK;
+		return value;
 	}
-	parseValue SQLNameWord::match(handle* h, const char*& sql)
+	SQLValue* SQLDBNameWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
 		std::string matchedWord;
 		const char* nameStart, * nameEnd;
 		uint16_t nameSize;
+		SQLDBNameValue* value = MATCH;
 		if (!getName(p, nameStart, nameSize, nameEnd))
 		{
 			LOG(ERROR) << m_comment << " do not match " << sql;
 			return NOT_MATCH;
 		}
-		if (*p != '\'' && *p != '`' && *p != '"')
+		if (m_parser != nullptr || needValue)
 		{
-			if (isKeyWord(nameStart, nameSize))
+			value = new SQLDBNameValue();
+			value->database.assign(nameStart, nameSize);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
+		LOG(ERROR) << m_comment << "match " << sql;
+		sql = nameEnd;
+		return value;
+	}
+	SQLValue* SQLTableNameWord::match(handle* h, const char*& sql, bool needValue)
+	{
+		const char* p = nextWord(sql);
+		const char* nameStart[2] = { 0 };
+		const char* nameEnd;
+		uint16_t nameSize[2] = {0};
+		SQLTableNameValue* value = MATCH;
+		if (!getName(p, nameStart[0], nameSize[0], nameEnd))
+		{
+			LOG(ERROR) << m_comment << " do not match " << sql;
+			return NOT_MATCH;
+		}
+		if (*(nameEnd + 1) == '.')
+		{
+			p = nameEnd + 2;
+			if (!getName(p, nameStart[1], nameSize[1], nameEnd))
 			{
 				LOG(ERROR) << m_comment << " do not match " << sql;
 				return NOT_MATCH;
 			}
 		}
-		if (m_parser != nullptr)
-			matchedWord = std::string(nameStart, nameSize);
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLTableNameValue();
+			if (nameStart[1] != nullptr)
+			{
+				value->database.assign(nameStart[0], nameSize[0]);
+				value->table.assign(nameStart[1], nameSize[1]);
+			}
+			else
+			{
+				value->table.assign(nameStart[0], nameSize[0]);
+			}
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
 		LOG(ERROR) << m_comment << "match " << sql;
 		sql = nameEnd;
-		success(h, matchedWord);
-		return OK;
+		return value;
 	}
-	parseValue SQLArrayWord::match(handle* h, const char*& sql)
+	SQLValue* SQLColumnNameWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
-		std::string matchedWord;
+		const char* nameStart[3] = { 0 };
+		const char* nameEnd;
+		uint16_t nameSize[3] = { 0 };
+		SQLColumnNameValue* value = MATCH;
+		if (!getName(p, nameStart[0], nameSize[0], nameEnd))
+		{
+			LOG(ERROR) << m_comment << " do not match " << sql;
+			return NOT_MATCH;
+		}
+		if (*(nameEnd + 1) == '.')
+		{
+			p = nameEnd + 2;
+			if (!getName(p, nameStart[1], nameSize[1], nameEnd))
+			{
+				LOG(ERROR) << m_comment << " do not match " << sql;
+				return NOT_MATCH;
+			}
+			if (*(nameEnd + 1) == '.')
+			{
+				p = nameEnd + 2;
+				if (!getName(p, nameStart[2], nameSize[2], nameEnd))
+				{
+					LOG(ERROR) << m_comment << " do not match " << sql;
+					return NOT_MATCH;
+				}
+			}
+		}
+
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLColumnNameValue();
+			if (nameStart[2] != nullptr)
+			{
+				value->database.assign(nameStart[0], nameSize[0]);
+				value->table.assign(nameStart[1], nameSize[1]);
+				value->columnName.assign(nameStart[2], nameSize[2]);
+			}
+			else if(nameStart[1] != nullptr)
+			{
+				value->table.assign(nameStart[0], nameSize[0]);
+				value->columnName.assign(nameStart[1], nameSize[1]);
+			}
+			else
+			{
+				value->columnName.assign(nameStart[0], nameSize[0]);
+			}
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
+		LOG(ERROR) << m_comment << "match " << sql;
+		sql = nameEnd;
+		return value;
+	}
+	SQLValue* SQLArrayWord::match(handle* h, const char*& sql, bool needValue)
+	{
+		const char* p = nextWord(sql);
+		SQLStringValue* value = MATCH;
 		if (*p != '\'' && *p != '"')
 		{
 			LOG(ERROR) << m_comment << " do not match " << sql;
@@ -127,18 +241,30 @@ namespace SQL_PARSER {
 		{
 			LOG(ERROR) << m_comment << " do not match " << sql;
 			return NOT_MATCH;
-		}		
-		if (m_parser != nullptr)
-			matchedWord = std::string(p + 1, end - p - 1);
+		}
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLStringValue(SQLValue::STRING_TYPE);
+			value->value.assign(p + 1, end - p - 1);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
 		LOG(ERROR) << m_comment << "match " << sql;
-
 		sql = end + 1;
-		success(h, matchedWord);
-		return OK;
+		return value;
 	}
-	parseValue SQLStringWord::match(handle* h, const char*& sql)
+	SQLValue* SQLStringWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
+		SQLStringValue* value = MATCH;
 		if (strncasecmp(m_word.c_str(), p, m_word.size()) != 0
 			|| (!isSpaceOrComment(p + m_word.size()) && p[m_word.size()] != '\0' && !isKeyChar(p[m_word.size()])))
 		{
@@ -147,13 +273,28 @@ namespace SQL_PARSER {
 		}
 		LOG(ERROR) << m_comment << "match " << sql;
 		sql = p + m_word.size();
-		success(h, m_word);
-		return OK;
+
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLStringValue(SQLValue::STRING_TYPE);
+			value->value.assign(m_word);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
+		return value;
 	}
-	parseValue SQLNumberWord::match(handle* h, const char*& sql)
+	SQLValue* SQLNumberWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
-		std::string matchedWord;
+		SQLStringValue* value = MATCH;
 		const char* n = p;
 		bool hasDot = false;
 		while (*n != '\0')
@@ -205,19 +346,30 @@ namespace SQL_PARSER {
 			}
 			n++;
 		}
-		if (m_parser != nullptr)
-			matchedWord = std::string(p, n - p);
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLStringValue(SQLValue::STRING_TYPE);
+			value->value.assign(p, n - p);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
 		LOG(ERROR) << m_comment << "match " << sql;
-
 		sql = n;
-		success(h, matchedWord);
-		return OK;
+		return value;
 	}
-	parseValue SQLAnyStringWord::match(handle* h, const char*& sql)
+	SQLValue* SQLAnyStringWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
-		std::string matchedWord;
 		const char* end = endOfWord(p);
+		SQLStringValue* value = MATCH;
 		if (end == nullptr)
 		{
 			LOG(ERROR) << m_comment << " do not match " << sql;
@@ -228,18 +380,30 @@ namespace SQL_PARSER {
 			LOG(ERROR) << m_comment << " do not match " << sql;
 			return NOT_MATCH;
 		}
-		if (m_parser != nullptr)
-			matchedWord = std::string(p, end - p);
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLStringValue(SQLValue::STRING_TYPE);
+			value->value.assign(p, end - p);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
 		LOG(ERROR) << m_comment << "match " << sql;
-
 		sql = end;
-		success(h, matchedWord);
-		return OK;
+		return value;
 	}
-	parseValue SQLBracketsWord::match(handle* h, const char*& sql)
+	SQLValue* SQLBracketsWord::match(handle* h, const char*& sql, bool needValue)
 	{
 		const char* p = nextWord(sql);
-		std::string matchedWord;
+		SQLStringValue* value = MATCH;
+
 		if (*p != '(')
 		{
 			LOG(ERROR) << m_comment << " do not match " << sql;
@@ -263,18 +427,28 @@ namespace SQL_PARSER {
 			LOG(ERROR) << m_comment << " do not match " << sql;
 			return NOT_MATCH;
 		}
-		if (m_parser != nullptr)
-			matchedWord.assign(p, end - p);
+		if (m_parser != nullptr || needValue)
+		{
+			value = new SQLStringValue(SQLValue::STRING_TYPE);
+			value->value.assign(p, end - p);
+			if (needValue)
+				value->ref++;
+			if (m_parser != nullptr)
+			{
+				value->ref++;
+				statusInfo* s = new statusInfo();
+				s->parserFunc = m_parser;
+				s->value = value;
+				h->addStatus(s);
+			}
+		}
 		LOG(ERROR) << m_comment << "match " << sql;
-
 		sql = end + 1;
-		success(h, matchedWord);
-		return OK;
+		return value;
 	}
-
-	parseValue SQLWordArray::match(handle* h, const char*& sql)
+	SQLValue* SQLWordArray::match(handle* h, const char*& sql, bool needValue)
 	{
-		parseValue rtv = OK;
+		SQLValue * rtv = nullptr;
 		bool matched = false;
 		const char* savePoint = sql, * beforeLoop = nullptr;
 		statusInfo* top = h->end;
@@ -287,13 +461,13 @@ namespace SQL_PARSER {
 				const char* str = nextWord(sql);
 				if (s == nullptr)
 					continue;
-				if ((rtv = s->match(h, str)) != OK)
+				if ((rtv = s->match(h, str)) != NOT_MATCH)
 				{
 					if (m_or)
 						continue;
 					if (s->m_optional)
 					{
-						rtv = OK;
+						rtv = nullptr;
 						continue;
 					}
 					else
@@ -306,13 +480,13 @@ namespace SQL_PARSER {
 						break;
 				}
 			}
-			if (rtv != OK)
+			if (rtv == NOT_MATCH)
 			{
 				if (m_loop && matched)
 				{
 					if (beforeLoop != nullptr)
 						sql = beforeLoop;
-					rtv = OK;
+					rtv = MATCH;
 				}
 				break;
 			}
@@ -324,7 +498,7 @@ namespace SQL_PARSER {
 				{
 					beforeLoop = sql;
 					const char* str = nextWord(sql);
-					if (m_loopCondition->match(nullptr, str) == OK)
+					if (m_loopCondition->match(nullptr, str) != NOT_MATCH)
 					{
 						sql = str;
 						continue;
@@ -337,7 +511,7 @@ namespace SQL_PARSER {
 				break;
 		} while (1);
 
-		if (rtv != OK) //rollback
+		if (rtv == NOT_MATCH) //rollback
 		{
 			for (statusInfo* s = top ? top->next : nullptr; s != nullptr;)
 			{
@@ -356,19 +530,117 @@ namespace SQL_PARSER {
 		else
 		{
 			LOG(ERROR) << m_comment << "  match " << savePoint;
-			if (m_parser != nullptr)
-			{
-				statusInfo* s = new statusInfo;
-				s->parserFunc = m_parser;
-				if (h->head == nullptr)
-					h->head = s;
-				else
-					h->end->next = s;
-				h->end = s;
-			}
 			if (m_sqlType != UNSUPPORT)
 				h->type = m_sqlType;
 		}
 		return rtv;
+	}
+	SQLWordExpressions::SQLWordExpressions(bool optional) :SQLWord(SQL_EXPRESSION, optional), m_parserFunc(nullptr), numberArgv(false), strWord(false), nameWord(false), func(nullptr)
+	{
+		func = new SQLWordFunction(false);
+	}
+	SQLWordExpressions::~SQLWordExpressions()
+	{
+		delete func;
+	}
+	SQLValue* SQLWordExpressions::match(handle* h, const char*& sql, bool needValue)
+	{
+		std::list<const char*> brackets;
+		SQLValue* value = MATCH;
+		const char* pos = nextWord(sql);
+		do {
+			while (*pos == '(')
+			{
+				brackets.push_back(pos);
+				pos = nextWord(pos + 1);
+			}
+			if ((value = match(nullptr, pos, needValue || m_parserFunc != nullptr)) != NOT_MATCH||
+				(value = func->match(nullptr, pos, needValue || m_parserFunc != nullptr)) != NOT_MATCH)
+			{
+				pos = nextWord(pos);
+			}
+		} while (*pos != '\0');
+		return NOT_MATCH;
+	}
+
+	SQLValue* SQLWordFunction::match(handle* h, const char*& sql, bool needValue)
+	{
+		SQLValue * name = nullptr;
+		const char* pos = nextWord(sql);
+		if ((name = asWord.match(nullptr, pos, needValue || m_parserFunc != nullptr)) == NOT_MATCH)
+			return NOT_MATCH;
+		pos = nextWord(pos);
+		if (*pos != '(')
+			return NOT_MATCH;
+		SQLFunctionValue* sfv = MATCH;
+
+		if (needValue || m_parserFunc != nullptr)
+		{
+			if (name == nullptr)
+				return NOT_MATCH;
+			sfv = new SQLFunctionValue();
+			sfv->funcName = static_cast<SQLStringValue*>(name)->value;
+			delete name;
+		}
+
+		pos = nextWord(pos + 1);
+		while (*pos != '\0')
+		{
+			if (*pos == ')')
+			{
+				sql = pos + 1;
+				if (needValue)
+					sfv->ref++;
+				if (h != nullptr&&m_parserFunc != nullptr)
+				{
+					statusInfo* s = new statusInfo();
+					sfv->ref++;
+					s->parserFunc = m_parserFunc;
+					s->value = sfv;
+					h->addStatus(s);
+				}
+				return sfv;
+			}
+			SQLValue* value = MATCH;
+			if ((value = expressionWord.match(h, pos, needValue)) != NOT_MATCH ||
+				(value = numberArgv.match(h, pos, needValue)) != NOT_MATCH ||
+				(value = nameWord.match(h, pos, needValue)) != NOT_MATCH ||
+				(value = strWord.match(h, pos, needValue)) != NOT_MATCH||
+				(value = match(nullptr, pos, needValue)) != NOT_MATCH)
+			{
+				if (sfv != nullptr)
+					sfv->argvs.push_back(value);
+			}
+			else
+			{
+				if (sfv != nullptr)
+					delete sfv;
+				return NOT_MATCH;
+			}
+
+			pos = nextWord(pos);
+			if (*pos == ',')
+			{
+				pos = nextWord(pos + 1);
+				if (*pos == ')')
+				{
+					if (sfv != nullptr)
+						delete sfv;
+					return NOT_MATCH;
+				}
+			}
+			else
+			{
+				if (*pos != ')')
+				{
+					if (sfv != nullptr)
+						delete sfv;
+					return NOT_MATCH;
+				}
+			}
+		}
+		if (sfv != nullptr)
+			delete sfv;
+		return NOT_MATCH;
 	}
 }
