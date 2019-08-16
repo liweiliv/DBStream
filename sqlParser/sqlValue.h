7bdd40daa695bd4086e413a7baad5d5da98d5d65
liweiliv <liwei960067495@sina.com>
2019-08-16 17:43:28 +0800

    Create sqlValue.h

---
+++ sqlParser/sqlValue.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <string>
+#include <list>
+#include "operationInfo.h"
+namespace SQL_PARSER {
+	class SQLValue {
+	public:
+		enum SQLValueType {
+			NUMBER_TYPE,
+			STRING_TYPE,
+			DB_NAME_TYPE,
+			TABLE_NAME_TYPE,
+			COLUMN_NAME_TYPE,
+			FUNCTION_TYPE,
+			EXPRESSION_TYPE
+		};
+		SQLValueType type;
+		uint32_t ref;
+		SQLValue(SQLValueType type) :type(type), ref(0){}
+		virtual ~SQLValue() {}
+	};
+	class SQLStringValue :public SQLValue {
+	public:
+		std::string value;
+		SQLStringValue(SQLValueType type) :SQLValue(type) {}
+	};
+	class SQLDBNameValue :public SQLValue {
+	public:
+		std::string database;
+		SQLDBNameValue() :SQLValue(DB_NAME_TYPE) {}
+	};
+	class SQLTableNameValue :public SQLValue {
+	public:
+		std::string database;
+		std::string table;
+		SQLTableNameValue() :SQLValue(TABLE_NAME_TYPE) {}
+	};
+	class SQLColumnNameValue :public SQLValue {
+	public:
+		std::string database;
+		std::string table;
+		std::string columnName;
+		SQLColumnNameValue() :SQLValue(COLUMN_NAME_TYPE) {}
+	};
+	class SQLFunctionValue :public SQLValue {
+	public:
+		std::string funcName;
+		std::list<SQLValue*> argvs;
+		SQLFunctionValue() :SQLValue(FUNCTION_TYPE) {}
+		~SQLFunctionValue()
+		{
+			for (std::list<SQLValue*>::iterator iter = argvs.begin(); iter != argvs.end(); iter++)
+				delete* iter;
+		}
+	};
+	class SQLExpressionValue :public SQLValue {
+	public:
+		SQLValue *rightValue;
+		OPERATOR opt;
+		SQLValue *leftValue;
+		SQLExpressionValue() :SQLValue(EXPRESSION_TYPE), rightValue(nullptr), leftValue(nullptr){}
+		~SQLExpressionValue()
+		{
+			if (rightValue)
+				delete rightValue;
+			if (leftValue)
+				delete leftValue;
+		}
+	};
+}
